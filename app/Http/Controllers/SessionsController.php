3c25f1877b86e0bbaa38135d33b6c7c1b9777f9f
xiehouyou <1617835576@qq.com>
2018-10-29 11:16:34 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -24,7 +24,22 @@ class SessionsController extends Controller
     		'email'=>'required|email|max:255',
     		'password'=>'required'
     	]);
-
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+           if(Auth::user()->activated) {
+            // 登录成功后的相关操作
+               session()->flash('success', '欢迎回来！');
+               return redirect()->intended(route('users.show', [Auth::user()]));
+           } else {
+               Auth::logout();
+               session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+               return redirect('/');
+           }
+       } else {
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
+       }
+    }
+/*
     	  if (Auth::attempt($credentials)) {
            // 登录成功后的相关操作
       	 session()->flash('success', '欢迎回来！');
@@ -34,7 +49,7 @@ class SessionsController extends Controller
        	 session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
        	 return redirect()->back();
        }
-    }
+    }*/
     
     public function destroy()
     {
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,15 @@
 Route::get('/','StaticPagesController@home')->name('home');
 Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
+
 Route::get('signup','UsersController@create')->name('signup');
 Route::Resource('users','UsersController');
+
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
+Route::get('signup/confirm/{token}','UsersController@confirmEmail')->name('confirm_email');
 /*Route::get('login','SessionsController@create')->name('login');
 Route::post('login','SessionsController@store')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
